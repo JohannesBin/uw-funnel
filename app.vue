b9a5a12f84b4f96d24d55406587b175b63f43327
JohannesBin <johannes.bingen@gmail.com>
2025-04-11 20:33:13 +0200

    Multiple updates: config changes and component modifications

--- app.vue
+++ app.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="min-h-screen bg-white text-black">
-    <DynamicMeta />
     <Navbar />
     <Hero />
     <StudiosVision />
@@ -12,16 +11,5 @@
 </template>
 
 <script setup lang="ts">
-// Import styles first
 import './assets/css/main.css'
-
-// Import components in specific order
-import DynamicMeta from '~/components/DynamicMeta.vue'
-import Navbar from '~/components/Navbar.vue'
-import Hero from '~/components/Hero.vue'
-import StudiosVision from '~/components/StudiosVision.vue'
-import Studios from '~/components/Studios.vue'
-import AboutFounders from '~/components/AboutFounders.vue'
-import Contact from '~/components/Contact.vue'
-import Footer from '~/components/Footer.vue'
 </script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -27,18 +27,20 @@ export default defineNuxtConfig({
         
         // Open Graph / Facebook
         { property: 'og:type', content: 'website' },
+        { property: 'og:url', content: 'https://studio.underwerk.no/' },
         { property: 'og:title', content: 'UWM Studios' },
         { property: 'og:description', content: 'UWM Studios - Studioer i hjertet av Oslo. Kongens gate 14. Åpning juni 2025.' },
-        { property: 'og:image', content: '/social/og-image.png' },
+        { property: 'og:image', content: 'https://studio.underwerk.no/social/og-image.png' },
         { property: 'og:image:width', content: '1200' },
         { property: 'og:image:height', content: '630' },
         { property: 'og:image:alt', content: 'Studioer - Kongens gate 14, Oslo - Juni 2025' },
         
         // Twitter
         { name: 'twitter:card', content: 'summary_large_image' },
+        { name: 'twitter:url', content: 'https://studio.underwerk.no/' },
         { name: 'twitter:title', content: 'UWM Studios' },
         { name: 'twitter:description', content: 'UWM Studios - Studioer i hjertet av Oslo. Kongens gate 14. Åpning juni 2025.' },
-        { name: 'twitter:image', content: '/social/og-image.png' },
+        { name: 'twitter:image', content: 'https://studio.underwerk.no/social/og-image.png' },
         { name: 'twitter:image:alt', content: 'Studioer - Kongens gate 14, Oslo - Juni 2025' }
       ],
       link: [
